39bc4e9062a6b1a41002d9d09603b312bea8b028
audreyjy <audrey.youngberg1@gmail.com>
2022-03-30 09:48:52 -0600

    committed

---
+++ Components/TeamsViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using Bowler_MySQL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bowler_MySQL.Components
+{
+    public class TeamsViewComponent : ViewComponent 
+    {
+        // get data for component here 
+        private BowlersDbContext _context { get; set; }
+
+        //Contructor
+        public TeamsViewComponent(BowlersDbContext temp)
+        {
+            _context = temp;
+        }
+
+
+        // invoke method 
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedType = RouteData.Values["bookCategory"];
+
+            // get category types 
+            var teams = _context.Bowlers
+                .Select(x => x.Team.TeamName)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return View(teams);
+        }
+
+
+    }
+}
